eeb4bd1c202a494e7b1b37c95f75013bf0663a52
Dilan Silva <sdilansilva@gmail.com>
2021-02-02 18:58:06 +0530

    Start to develop Enter Email Screen

---
+++ src/pages/EnterEmail/EnterEmail.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import HeaderComponent from '../HeaderComponent';
+import Lable from '../Sign-Up/Lable';
+import Input from '../Sign-Up/Input';
+
+const EnterEmail = () => {
+    return(
+        <div></div>
+    );
+}
+
+export default EnterEmail;
