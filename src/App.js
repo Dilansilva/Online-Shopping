ff13f27c71496faeb8dac842f4d712169e52a9f3
Dilan Silva <sdilansilva@gmail.com>
2021-02-13 21:24:07 +0530

    Home Page for Buyer is done but have to implement cart and have issue with showing images

--- src/App.js
+++ src/App.js
@@ -4,11 +4,12 @@ import SignUp from './pages/Sign-Up/Sign-Up';
 import EmailVerification from './pages/Email-Verification/EmailVerification';
 import EnterEmail from './pages/EnterEmail/EnterEmail';
 import ChangePassword from './pages/ChangePassword/ChangePassword';//import Change Password Web Page
+import HomeBuyer from './pages/Home(Buyer)/HomeBuyer';//home page for buyer
 
 const App = () => {
   return (
     <div className="App">
-       <ChangePassword/>
+       <HomeBuyer/>
     </div>
   );
 }
--- src/pages/Home(Buyer)/HomeBuyer.js
+++ src/pages/Home(Buyer)/HomeBuyer.js
@@ -1,10 +1,36 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import {Navbar,
+        Container,
+        Row,
+        Col,
+        Image} from 'react-bootstrap';
+
 
 const HomeBuyer = () => {
     return(
         <div>
-            <text>This is the home Screen For Buyer</text>
+                <Navbar expand="lg" variant="light" bg="light">
+                    <Navbar.Brand href="#">Online Shopping</Navbar.Brand>
+                </Navbar>
+                   <div class="py-5">
+                        <Container fluid>
+                            <Row>
+                                <Col sm>
+                                    <Image src={require("../../images/41vMYgD92xL.jpg")} rounded/>
+                                </Col>
+                                <Col sm>
+                                    <Image src={require("../../images/817Woygy77L._AC_UL320_.jpg")} rounded/>
+                                </Col>
+                                <Col sm>
+                                    <Image src={require("../../images/81jENTZX6UL._AC_UL320_.jpg")} rounded/>
+                                </Col>
+                                <Col sm>
+                                    <Image src={require("../../images/81sacygDqDL._AC_UL320_.jpg")}rounded/>
+                                </Col>
+                            </Row>
+                        </Container>
+                   </div>
         </div>
     );
 }
