76d76fb7b11768874603954053a8882a716bbe2f
Dilan Silva <dilan.silva@colombo.rezgateway.com>
2021-04-20 17:17:24 +0530

    cart icon is added

--- src/pages/HeaderComponent.js
+++ src/pages/HeaderComponent.js
@@ -8,6 +8,8 @@ import '../css/HeaderComponent.css';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';//icon for shoppig cart
+
 const HeaderComponent = () => {
   
     return(
@@ -18,7 +20,10 @@ const HeaderComponent = () => {
         <Nav className="ml-auto">
           <Nav.Link href="/signup">Sign-up</Nav.Link>
           <Nav.Link href="/">Sign-in</Nav.Link>
-         
+            <Nav.Link href="/cart">
+              <ShoppingCartIcon/>
+              <span>0</span>
+            </Nav.Link>
         </Nav>
        
       </Navbar.Collapse>
