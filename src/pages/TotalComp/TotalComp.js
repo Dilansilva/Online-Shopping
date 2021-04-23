2119bbd2a2f912365a7af633c1db830d0cb50054
Dilan Silva <dilan.silva@colombo.rezgateway.com>
2021-04-23 13:06:49 +0530

    Total component is done and done some changes

--- src/pages/TotalComp/TotalComp.js
+++ src/pages/TotalComp/TotalComp.js
@@ -1,21 +1,36 @@
 import React, { useState } from "react";
-import { Container, Row, Col } from "react-bootstrap";
+import { Container, Row, Col, Button } from "react-bootstrap";
 
 const TotalComp = () => {
-  const [Total, setTotal] = useState(2000);
+  const [Total, setTotal] = useState(0.0);
   return (
     <div>
+      <br />
       <Container
         className="block-example border border-primary"
         style={{ alignItems: "center", borderRadius: "20px" }}
       >
         <Row>
-          <Col xs={3} md={7}>
-            <h1></h1>
+          <Col xs={3} md={3}>
+            <Button
+              variant="success"
+              style={{ marginTop: "10px", marginLeft: "50px" }}
+            >
+              Clear THe Cart
+            </Button>
+          </Col>
+
+          <Col xs={3} md={3}>
+            <Button
+              variant="success"
+              style={{ marginTop: "10px", marginLeft: "50px" }}
+            >
+              Pay Here
+            </Button>
           </Col>
 
           <Col xs={3} md={5}>
-            <h1>Totale Price : ${Total}</h1>
+            <h1>Total Price : ${Total}</h1>
           </Col>
         </Row>
       </Container>
