455316e45c99523cdf66d1aa1f11304590ec91f4
Dilan Silva <sdilansilva@gmail.com>
2021-05-17 17:06:49 +0530

    cartDetals file is created

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import {createStore} from 'redux';
+
+const store = createStore();
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
